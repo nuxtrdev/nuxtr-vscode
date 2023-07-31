737c1a4a9057365a2c3870c661bdfe6074913e5b
Adham Farrag <adham.farrag@hotmail.com>
2023-07-31 18:37:52 +0800

    fix: supabase public key

--- ui/src/lib/supabaseClient.ts
+++ ui/src/lib/supabaseClient.ts
@@ -1,10 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://ozgtbqizepstargxfqcm.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im96Z3RicWl6ZXBzdGFyZ3hmcWNtIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY4MjgzODExOCwiZXhwIjoxOTk4NDE0MTE4fQ.TZ8sWLB6ru1tULm3s3C96V1xaKJRjbpQii-MscyiOZ0";
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_KEY;
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
-
-
-
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
