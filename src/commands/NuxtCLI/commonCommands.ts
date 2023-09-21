1dd9ad280ed48bf60489f59da84d904f34059e43
Adham Farrag <adham.farrag@hotmail.com>
2023-09-21 22:00:17 +0800

    fix: cleanup command typo

--- src/commands/NuxtCLI/commonCommands.ts
+++ src/commands/NuxtCLI/commonCommands.ts
@@ -6,7 +6,7 @@ const runCommand = pm ? pm.runCommand : 'npx';
 const nuxtDev = () => newTerminal('Nuxi: Dev', `${runCommand} nuxi dev`, `${projectRootDirectory()}`)
 const nuxtBuild = () => newTerminal('Nuxi: Build', `${runCommand} nuxi build`, `${projectRootDirectory()}`)
 const nuxtGenerate = () => newTerminal('Nuxi: Build', `${runCommand} nuxi generate`, `${projectRootDirectory()}`)
-const nuxtCleanUp = () => newTerminal('Nuxi: Cleanup', `${runCommand} nuxi clean`, `${projectRootDirectory()}`)
+const nuxtCleanUp = () => newTerminal('Nuxi: Cleanup', `${runCommand} nuxi cleanup`, `${projectRootDirectory()}`)
 const nuxtAnalyze = () => newTerminal('Nuxi: Analyze', `${runCommand} nuxi analyze`, `${projectRootDirectory()}`)
 const nuxtInfo = () => newTerminal('Nuxi: Info', `${runCommand} nuxi info`, `${projectRootDirectory()}`)
 
