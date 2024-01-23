85cbf714b38e1a1d2c2e96866e1d0707b8da67cd
Adham Farrag <adham.farrag@hotmail.com>
2024-01-24 05:24:56 +0800

    feat: Volar TakeOver Mode watcher

--- src/watchers/index.ts
+++ src/watchers/index.ts
@@ -1,9 +1,11 @@
 import filesWatcher from './files'
 import { snippetsConfigWatcher, templatesConfigWatcher, piniaConfigWatcher } from './config'
 
+import { checkTakeOverMode} from './vscode'
 export {
     filesWatcher,
     snippetsConfigWatcher,
     templatesConfigWatcher,
-    piniaConfigWatcher
+    piniaConfigWatcher,
+    checkTakeOverMode
 }
